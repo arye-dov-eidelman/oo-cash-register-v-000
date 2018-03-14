33e03e0e152b1b426390611f36dda4b6b4a31333
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-03-14 01:02:10 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@ class CashRegister
 attr_reader :total, :discount
   def initialize(discount=nil)
     @total = 0
-    @discounts = discount if discount
+    @discount = discount if discount
   end
 
 end
